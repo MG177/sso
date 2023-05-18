d6d961615fb4744275e0f51be991d82dbd2aeb0a
MG177 <mg.terok77@gmail.com>
2023-05-18 20:46:08 +0800

    Update: - created modal for usermanager - fixed bug in view details paginator - added some transition animation

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -14,7 +14,10 @@ export default function Header() {
   return (
     <div className="flex flex-row top-0 justify-between sticky px-5 py-5 bg-bw1 h-fit rounded-b-main shadow offset-y-1px blur-14 font-semibold font-main text-bw3 min-w-fit">
       <div className="flex flex-row gap-4 text-title1">
-        <Link to="/dashboard/log" className="flex flex-col align-middle gap-1">
+        <Link
+          to="/dashboard/log"
+          className="flex flex-col align-middle gap-1 mx-1 transition ease-in-out hover:scale-110"
+        >
           <div>User Log</div>
           {location.pathname.startsWith('/dashboard/log') && (
             <hr className="bg-bw4 h-0.5 border-0"></hr>
@@ -22,7 +25,7 @@ export default function Header() {
         </Link>
         <Link
           to="/dashboard/userManager"
-          className="flex flex-col align-middle gap-1"
+          className="flex flex-col align-middle gap-1 mx-1 transition ease-in-out hover:scale-110"
         >
           <div>User Manager</div>
           {location.pathname.startsWith('/dashboard/userManager') && (
@@ -31,7 +34,7 @@ export default function Header() {
         </Link>
         <Link
           to="/dashboard/appManager"
-          className="flex flex-col align-middle gap-1"
+          className="flex flex-col align-middle gap-1 mx-1 transition ease-in-out hover:scale-110"
         >
           <div>App Manager</div>
           {location.pathname.startsWith('/dashboard/appManager') && (
--- src/page/Dashboard.jsx
+++ src/page/Dashboard.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Header from '../component/Header';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import log from '../dummy/userLog';
 import data from '../dummy/userData';
 import app from '../dummy/appData';
@@ -24,6 +24,18 @@ export default function Dashboard() {
 
 function Log() {
   const dt = useRef(null);
+  const status = (rowData) => {
+    const { status } = rowData;
+    return (
+      <div
+        className={`${
+          status.isSuccess ? 'text-green-500' : 'text-red-500'
+        } font-main font-semibold`}
+      >
+        {status.message}
+      </div>
+    );
+  };
   return (
     <div>
       <div className="w-full h-fit bg-bw2">
@@ -56,6 +68,7 @@ function Log() {
           <Column field="name" header="Name" />
           <Column field="app" header="App" />
           <Column field="date" header="Date & Time" />
+          <Column body={status} header="Date & Time" />
         </DataTable>
       </div>
     </div>
@@ -64,6 +77,52 @@ function Log() {
 
 function UserManager() {
   const dt = useRef(null);
+  const [user, setUser] = useState({});
+  const [passwordEditor, setPasswordEditor] = useState(false);
+  const dialog = document.querySelector('#dialog');
+  if (dialog !== null) {
+    dialog.addEventListener('click', (e) => {
+      const dialogDimensions = dialog.getBoundingClientRect();
+      if (
+        e.clientX < dialogDimensions.left ||
+        e.clientX > dialogDimensions.right ||
+        e.clientY < dialogDimensions.top ||
+        e.clientY > dialogDimensions.bottom
+      ) {
+        dialog.close();
+      }
+    });
+  }
+
+  const closeModal = () => {
+    if (dialog !== null) {
+      dialog.close();
+    }
+  };
+
+  const handlePasswordEditor = () => {
+    setPasswordEditor(!passwordEditor);
+  };
+
+  const handleViewDetails = (rowData) => {
+    const { nim, id, name, role } = rowData;
+    const dialog = document.querySelector('#dialog');
+    console.log(passwordEditor);
+    return (
+      <div className="flex justify-center w-fit">
+        <Button
+          label="View"
+          className="p-button-rounded p-button-text"
+          onClick={() => {
+            console.log(nim);
+            setUser({ nim, id, name, role });
+            dialog.showModal();
+          }}
+        />
+      </div>
+    );
+  };
+
   return (
     <div>
       <div className="w-full h-fit bg-bw2">
@@ -79,7 +138,7 @@ function UserManager() {
       <div className="h-[70vh] rounded-main shadow-lg border-bw2 overflow-hidden">
         <DataTable
           ref={dt}
-          value={log}
+          value={data}
           paginator
           rows={15}
           rowsPerPageOptions={[15, 25, 50]}
@@ -96,10 +155,62 @@ function UserManager() {
           <Column field="id" header="No." />
           <Column field="nim" header="NIP/NIM" className="font-bold" />
           <Column field="name" header="Name" />
-          <Column field="app" header="App" />
-          <Column field="date" header="Date & Time" />
+          <Column field="role" header="role" />
+          <Column body={handleViewDetails} header="View Details" />
         </DataTable>
       </div>
+      <dialog className="h-fit rounded-main w-fit p-0" id="dialog">
+        <div className="flex flex-row justify-between items-center gap-52 bg-bw2 py-3.5 px-6 ">
+          <span className="font-semibold font-main text-bw3 text-h5">
+            User Details
+          </span>
+          <i
+            className="pi pi-times cursor-pointer transition ease-in-out hover:scale-110"
+            onClick={closeModal}
+          />
+        </div>
+        <div className="p-5">
+          <div className="grid grid-cols-2 font-main text-title1 text-bw3 font-semibold ">
+            <div>Nim</div>
+            <div>: {user.nim}</div>
+            <div>Name</div>
+            <div>: {user.name}</div>
+            <div>Role Title:</div>
+            <div>: {user.role}</div>
+          </div>
+          <div className=" my-3">
+            <div
+              className={`flex flex-col gap-3 ${!passwordEditor && 'hidden'}`}
+            >
+              <hr className="bg-bw4 h-0.5 border-0 w-full"></hr>
+              <input
+                type="password"
+                placeholder="Password"
+                className="rounded-main px-3.5 py-3 bg-bw1 focus:outline-none border-bw3 border-2 border-solid"
+              />
+              <input
+                type="password"
+                placeholder="Confirm Password"
+                className="rounded-main px-3.5 py-3 bg-bw1 focus:outline-none border-bw3 border-2 border-solid"
+              />
+            </div>
+
+            <div className="grid grid-cols-1">
+              <hr className="bg-bw4 h-0.5 border-0 w-full"></hr>
+              <i
+                className={`pi pi-chevron-down cursor-pointer w-fit justify-self-end ${
+                  passwordEditor && 'rotate-180 order-first'
+                }`}
+                onClick={handlePasswordEditor}
+              />
+            </div>
+          </div>
+          <div className="flex flex-row justify-end">
+            <Button label="Cancel" isFill={false} />
+            <Button label="Confirm" isFill={true} />
+          </div>
+        </div>
+      </dialog>
     </div>
   );
 }
@@ -127,4 +238,26 @@ function Search() {
       />
     </div>
   );
+}
+
+function Button({ label, isFill, onClick }) {
+  if (isFill) {
+    return (
+      <button
+        onClick={onClick}
+        className="flex flex-row py-1.5 px-4 justify-center items-center bg-bw3 rounded-main font-main text-title1 text-bw2 font-semibold border-2 hover:brightness-110 active:brightness-90 select-none"
+      >
+        {label}
+      </button>
+    );
+  } else {
+    return (
+      <button
+        onClick={onClick}
+        className="flex flex-row py-1.5 px-4 justify-center items-center bg-bw1 rounded-main font-main text-title1 text-bw3 font-semibold border-bw3 border-2 hover:brightness-105 active:brightness-90 select-none"
+      >
+        {label}
+      </button>
+    );
+  }
 }
